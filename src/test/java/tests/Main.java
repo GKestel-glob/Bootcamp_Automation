70a24c3139ecbbad2b3409f9ca45b25426be0865
GKestel-ams <GKestel@loyalty.com>
2023-09-12 17:25:45 -0300

    Added first test

---
+++ src/test/java/tests/Main.java
@@ -0,0 +1,17 @@
+package tests;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Main {
+		
+	public static void main(String[] args) {
+		
+		WebDriver driver;
+		System.setProperty("webdriver.chrome.driver", "./src/test/resources/chromedriver/chromedriver.exe");  
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.globant.com/");	
+		// Uncomment this line to automatically close the browser at the end of the test
+		//driver.quit();  
+	}
+}
